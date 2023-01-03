9bfc55f5a6b355c4e4b66f64a8a7c7b36a450e99
Otakuchan21 <78582741+Otakuchan21@users.noreply.github.com>
2023-01-03 14:08:12 +0000

    attempted to clean the code a bit

--- auto_car/scripts/path_planning.py
+++ auto_car/scripts/path_planning.py
@@ -18,17 +18,15 @@ class pathPlanning():
         self.initial_x = 0.0
         self.initial_y = 0.0
         self.initial_angular_z = 0.0
-        self.flag = True
+        self.times_ran = 0
+        self.calc_count = 0
+        self.intercept = 0.0
         self.pub_msg = Twist()
         self.velocityValue()
         self.path = 2
-        self.flag2 = True
         self.distance = 0.0
         self.rate = rospy.Rate(1)
         self.line = 'straight'
-        self.flag3 = True
-        self.flag4 = True
-        self.flag5 = True
 
     def subCallback(self, data):
         # stores all position values in the relevant variables
@@ -36,59 +34,58 @@ class pathPlanning():
         objOrient = data.pose.pose.orientation
         quaternion = [objOrient.x, objOrient.y, objOrient.z, objOrient.w]
         deg_x, deg_y, deg_z = euler_from_quaternion(quaternion)
-        # flag is used to store the initial values.
-        if self.flag == True:
+        # conter variable is used to store the initial values.
+        if self.times_ran == 0:
             self.initial_x = objLinear.x 
             self.initial_y = objLinear.y
-            self.flag = False
+            self.times_ran+=1
         else:
             self.current_x = objLinear.x 
             self.current_y = objLinear.y 
             self.current_angular_z = deg_z
 
     def pathDefinition(self):
-        # this routine makes the car travel the shortest path
+        # this routine makes the car travel the pre-defined path
         if abs(self.distance)<0.2:
             self.carControl()
             self.distance = self.distanceCalculation(self.line)
             #print("distance during straight line traversal: ", self.distance)
-            #print(self.line)
         elif abs(self.distance)>=0.2 and abs(self.distance)<0.4:
-            if self.path == 1 and self.flag4 == True:
+            # this conditional block reorients the car and resets initial values for calculations
+            if self.path == 1 and self.times_ran == 2:
                 self.service_call(self.current_angular_z+30)
-                self.flag4 = False
-            elif self.path == 2 and self.flag4 == True:
-                self.service_call(self.current_angular_z-30)
-                self.flag4 = False
-            if self.flag3 == True:
                 self.initial_x = self.current_x
                 self.initial_y = self.current_y
-                self.flag3 = False
+                
+            elif self.path == 2 and self.times_ran == 2:
+                self.service_call(self.current_angular_z-30)
+                self.initial_x = self.current_x
+                self.initial_y = self.current_y
+
             self.line = 'slant'
             self.distance = self.distanceCalculation(self.line)
             #print('linear distance of car on slant line: ', self.distance)
             self.carControl()
             #print('linear velocity: ', self.pub_msg.linear.x)
         elif self.path == 2 and abs(self.distance)>=0.4 and abs(self.distance)<0.6:
-            if self.flag5 == True:
+            if self.times_ran == 3:
                 self.service_call(90)
                 self.initial_x = self.current_x
                 self.initial_y = self.current_y
-                self.flag5 = False
             self.carControl()
-            self.distance = self.distance = self.distanceCalculation(self.line) + 0.2
+            self.distance = self.distanceCalculation(self.line) + 0.2
             
         else:
+            # reset all velocity commands
             self.velocityValue()
             self.pub.publish(self.pub_msg)
 
 
     def carControl(self):
         # this function checks for deviation from line and send cmd velocity accordingly
-        if self.flag2 == True:
+        if self.times_ran == 1:
             # this service call is for fixing the orientation of the car
             self.service_call(0.0)
-            self.flag2 = False
             self.initial_angular_z = self.current_angular_z
         # this if block ensures that the right deviation is used for the line being traversed.
         if self.line == 'straight':
@@ -104,7 +101,7 @@ class pathPlanning():
             distance = self.distanceCalculation('deviation2')
             travel_range_ul = 0.10
             travel_range_ll = -0.10
-        # this if block checks if the car is moving in the correct direction.
+        # this if block checks if the car is moving whithin the error limits.
         if distance < travel_range_ul and distance > travel_range_ll:
             self.pub_msg.linear.x = 0.1
             self.pub_msg.angular.z = 0.0
@@ -121,8 +118,9 @@ class pathPlanning():
     def distanceCalculation(self, line):
         if self.path == 1:
             if line == 'deviation':
-                c = self.equationOfLine()
-                num = (self.current_x*math.sqrt(3)/3) + c -self.current_y
+                if self.calc_count == 0: 
+                    self.intercept = self.equationOfLine()
+                num = (self.current_x*math.sqrt(3)/3) + self.intercept -self.current_y
                 print("numerator for diatance calculation: ", num)
                 den = math.sqrt(4/3)
                 print("denominator for diatance calculation: ", den)
@@ -130,8 +128,9 @@ class pathPlanning():
         elif self.path == 2:
             # will define the second path later first need to fix the deviation calcultion of the lines.
             if line == 'deviation':
-                c = self.equationOfLine()
-                num = (self.current_x*math.sqrt(3)/3) - c +self.current_y
+                if self.calc_count == 0:
+                    self.intercept = self.equationOfLine()
+                num = (self.current_x*math.sqrt(3)/3) - self.intercept +self.current_y
                 print("numerator for diatance calculation: ", num)
                 den = math.sqrt(4/3)
                 print("denominator for diatance calculation: ", den)
@@ -155,11 +154,13 @@ class pathPlanning():
         else:
             intercept = self.current_y + math.sqrt(3)*self.current_x/3
         print("intercept: ", intercept)
+        self.calc_count+=1
         return intercept
 
     def service_call(self, target):
         rospy.wait_for_service('orientation_fix')
         response = self.request(target)
+        self.times_ran += 1
         
     def velocityValue(self):
         self.pub_msg.linear.y = 0
